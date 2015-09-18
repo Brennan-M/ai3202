1574cd9c543acbc2f1ce9596db2df401033da49a
Brennan McConnell <brmc3887@colorado.edu>
2015-09-17 23:02:09 -0600

    almost done

--- Assignment3/Maze.py
+++ Assignment3/Maze.py
@@ -4,6 +4,103 @@
 
 import sys
 
+# Node Class used in our Maze Solver
+class Node(object):
+
+	def __init__(self, location, distanceToStart = 0, parent = None):
+		self.location = location
+		self.distanceToStart = distanceToStart
+		self.parent = parent
+
+	def getLocation(self):
+		return self.location
+
+	def getDistanceToStart(self):
+		return self.distanceToStart
+
+	def getParent(self):
+		return self.parent
+
+	def setDistanceToStart(self, h, isMountain, endP):
+		pLoc = self.parent.getLocation()
+		stepDistance = abs(pLoc[0] - self.location[0]) + abs(pLoc[1] - self.location[1])
+		if stepDistance == 1:
+			self.distanceToStart = 10 + self.parent.getDistanceToStart()
+		else:
+			self.distanceToStart = 14 + self.parent.getDistanceToStart()
+
+		if isMountain == 1:
+			self.distanceToStart += 10
+
+		if h == 1:
+			self.distanceToStart += abs(endP[0] - self.location[0]) + abs(endP[1] - self.location[1])
+
+	def setParent(self, pNode):
+		self.parent = pNode
+
+
+
+def solveMaze(worldMaze, heuristic):
+	endPosition = (0, len(worldMaze[0]) - 1)
+
+	start = (len(worldMaze) - 1, 0) #Assigns our starting index to the bottom left
+	startNode = Node(start, 0)
+
+	openOptions = []
+	visited = {}
+	openOptions.append(startNode)
+	count = 0
+
+	while (len(openOptions) > 0):
+
+		tempNode = getMinimumNode(openOptions)
+		visited[tempNode.getLocation()] = True
+		count += 1
+		
+		for node in openOptions:
+			if node.getLocation() == tempNode.getLocation():
+				openOptions.remove(node)
+
+		if (tempNode.getLocation() != endPosition):
+
+			currentLocation = tempNode.getLocation()
+
+			for i in range(currentLocation[0] - 1, currentLocation[0] + 2):
+				for j in range(currentLocation[1] - 1, currentLocation[1] + 2):
+					
+					if (i >= 0 and i < len(worldMaze)):
+						if (j >= 0 and j < len(worldMaze[0])):
+							if (visited.get((i,j), False) != True): # We have not visited it yet
+								if (worldMaze[i][j] != 2):
+									adjNode = Node((i,j))
+									adjNode.setParent(tempNode)
+									adjNode.setDistanceToStart(heuristic, worldMaze[i][j], endPosition)
+									
+									alterExisting = False
+									for node in openOptions:
+										if node.getLocation() == (i,j):
+											if node.getDistanceToStart() > adjNode.getDistanceToStart():
+												alterExisting = True
+												break
+
+									if alterExisting == True:
+										node.distanceToStart = adjNode.getDistanceToStart()
+									else:
+										openOptions.append(adjNode)
+
+		else:
+			traceInformation(tempNode, worldMaze, count)
+			return
+
+	print "no Solution Found"
+
+def getMinimumNode(openOptions):
+	result = openOptions[0]
+	for node in openOptions:
+		if node.getDistanceToStart() < result.getDistanceToStart():
+			result = node
+
+	return result
 
 # We are forced to assume valid file input.
 def getArgs():
@@ -28,27 +125,73 @@ def getArgs():
 
 def constructWorld(fileName):
 	worldMaze = []
-	worldFile = open(fileName, "r");
+	worldFile = open(fileName, "r")
 	for line in worldFile:
 		worldMaze.append([int(i) for i in line.split()])
 
 	return worldMaze
 
 def printMaze(worldMaze):
-	print("Maze:")
+	print("Path:")
 	for i in worldMaze:
-		print i
+		tmpArr = [str(x) for x in i]
+		print " ".join(tmpArr)
 
 def printHeuristic(h):
-	print("")
 	if h == 1:
 		print("Heuristic: Manhattan")
 	elif h == 2:
 		print("Heuristic: Custom")
 
+def traceInformation(node, maze, count):
+	cost = 0
+	path = []
+	heuristicTrace = []
+
+	print "\nTotal Heuristic cost: ", node.getDistanceToStart()
+	
+	tmp_ptr = node
+	while (tmp_ptr):
+		path.append(tmp_ptr.getLocation())
+		if not tmp_ptr.getParent() == None:
+			stepDistance = abs(tmp_ptr.getParent().getLocation()[0] - tmp_ptr.getLocation()[0]) + abs(tmp_ptr.getParent().getLocation()[1] - tmp_ptr.getLocation()[1])
+			if stepDistance == 1:
+				cost += 10
+			elif stepDistance == 2:
+				cost += 14
+			if worldMaze[tmp_ptr.getLocation()[0]][tmp_ptr.getLocation()[1]] == 1:
+				cost += 10
+			heuristicTrace.append(tmp_ptr.getDistanceToStart())
+		tmp_ptr = tmp_ptr.getParent()
+
+	heuristicTrace.append(0)
+
+	print "Total Cost: ", cost
+	print "Nodes Evaluted: ", count
+
+	print "\nPath:"
+	for i in range(len(path) - 1, -1, -1):
+		print path[i], heuristicTrace[i]
+	print("")
+
+	for x,y in path:
+		maze[x][y] = 'X'
+
+	printMaze(maze)
+
 
 if __name__ == "__main__":
 	(worldMaze, heuristic) = getArgs()
-	printMaze(worldMaze)
 	printHeuristic(heuristic)
+	solveMaze(worldMaze, heuristic)
 	print ("\n-----------------------------------\n")
+
+
+
+
+
+
+
+
+
+
