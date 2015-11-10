6492186bf465c9588e95b4b69f6fa090ecfa500d
Brennan McConnell <brmc3887@colorado.edu>
2015-11-10 15:47:37 -0700

    Pretty much done

--- Assignment8/hmmBuilder.py
+++ Assignment8/hmmBuilder.py
@@ -2,3 +2,109 @@
 # Hidden Markov Model Part 1 #
 #	   Brennan McConnell	 #
 ##############################
+
+import sys
+
+TRANSITION = 0
+EMISSION = 1
+MARGINAL = 2
+
+class HMM(object):
+
+	def __init__(self):
+		self.states = {}
+		for i in range(97, 123):		
+			self.states[chr(i)] = ({}, {}, {})
+		self.states["_"] = ({}, {}, {})
+
+	def buildFromData(self, fileName):
+		f = open(fileName)
+		infoArr = []
+		for line in f:
+			infoArr.append((line[0], line[2]))
+	
+		# Emission Probabilities
+		for state in self.states.keys():
+
+			stateCount = 0.0
+			outputCount = {}
+			transitionCount = {}
+
+			for i in range(0, len(infoArr) - 1):
+				if (infoArr[i][0] == state):
+					stateCount += 1
+
+					# Emission Probabilities
+					if outputCount.has_key(infoArr[i][1]):
+						outputCount[infoArr[i][1]] += 1
+					else:
+						outputCount[infoArr[i][1]] = 1
+
+
+					# Transition Probabilities
+					if transitionCount.has_key(infoArr[i+1][0]):
+						transitionCount[infoArr[i+1][0]] += 1
+					else:
+						transitionCount[infoArr[i+1][0]] = 1
+
+
+			for s1 in self.states.keys():
+				if not self.states[state][EMISSION].has_key(s1):
+					self.states[state][EMISSION][s1] = round(1.0/(stateCount+27), 5)
+
+			for s2 in self.states.keys():
+				if not self.states[state][TRANSITION].has_key(s2):
+					self.states[state][TRANSITION][s2] = round(1.0/(stateCount+27), 5)
+
+
+			for key, value in outputCount.items():
+				self.states[state][EMISSION][key] = round((value+1)/(stateCount+27), 5)
+
+			for key, value in transitionCount.items():
+				self.states[state][TRANSITION][key] = round((value+1)/(stateCount+27), 5)
+
+			self.states[state][MARGINAL][state] = round(stateCount/len(infoArr), 5)
+
+
+	def printHMM(self):
+		print "---------------------------------------"
+
+		print "\n"
+		print "Marginal Probabilities"
+		print "---------------------------------------"
+		for state in self.states.keys():
+			print state, ":", self.states[state][MARGINAL][state]
+
+		print "\n"
+		print "Emission Probabilities"
+		print "---------------------------------------"
+		for state in self.states.keys():
+			for evidence, value in self.states[state][EMISSION].items():
+				print "P(", evidence, "|", state, ") =", value
+
+		print "\n"
+		print "Transition Probabilities"
+		print "---------------------------------------"
+		for state in self.states.keys():
+			for stateNext, value in self.states[state][TRANSITION].items():
+				print "P(", stateNext, "|", state, ") =", value
+
+		print "\n"
+		print "---------------------------------------"
+
+
+
+
+if __name__ == "__main__":
+	hmm = HMM()
+	fname = sys.argv[1]
+	hmm.buildFromData(fname)
+	hmm.printHMM()
+
+
+
+
+
+
+
+
